644ea382ebed3f341a1dc2790617044d42992aec
Anthony <mountindew@yandex.ru>
2018-09-07 23:15:30 +0400

    WebChat v3.0

--- src/test/java/web/app/chat/MainApplicationTest.java
+++ src/test/java/web/app/chat/MainApplicationTest.java
@@ -1,38 +1,22 @@
 package web.app.chat;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import web.app.chat.controller.MainController;
 
-import java.net.URL;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.http.HttpStatus.OK;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MainApplicationTest {
 
-    @LocalServerPort
-    private int port;
-
-    private URL base;
-
     @Autowired
     private MainController mainController;
-
-    @Before
-    public void setUp() throws Exception {
-        this.base = new URL("http://localhost:" + port + "/");
-    }
 
     @Test
     public void loadController(){
@@ -49,12 +33,7 @@ public class MainApplicationTest {
         assertThat(loginResponse).isEqualByComparingTo(OK);
     }
 
-    @Test
-    public void getLogin() {
-        ResponseEntity<String> response = mainController.login("Test");
-
-//        assertThat();
-    }
+    /* TODO */
 
 }
 
